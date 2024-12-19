30bcb756a2102bfaf568d6d3c58917845d7f7056
qureshikaif <qureshik488@gmail.com>
2024-12-19 19:08:36 +0500

    fix: fixing backend vercel deployment

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 // server.js
 
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import authRoutes from './routes/auth.route.js';
-import gearPackRoutes from './routes/gear-pack.route.js';
-import gearListRoutes from './routes/gear-list.route.js';
-import reminderRoutes from './routes/reminder.route.js';
-import groupPackingRoutes from './routes/group-packing.route.js';
-import errorHandler from './middlewares/error.middleware.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import gearPackRoutes from "./routes/gear-pack.route.js";
+import gearListRoutes from "./routes/gear-list.route.js";
+import reminderRoutes from "./routes/reminder.route.js";
+import groupPackingRoutes from "./routes/group-packing.route.js";
+import errorHandler from "./middlewares/error.middleware.js";
 
 // Load environment variables
 dotenv.config();
@@ -21,12 +21,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from API" });
+});
+
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/gearpacks', gearPackRoutes);
-app.use('/api/gearlists', gearListRoutes);
-app.use('/api/reminders', reminderRoutes);
-app.use('/api/grouppacking', groupPackingRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/gearpacks", gearPackRoutes);
+app.use("/api/gearlists", gearListRoutes);
+app.use("/api/reminders", reminderRoutes);
+app.use("/api/grouppacking", groupPackingRoutes);
 
 // Error Handling Middleware
 app.use(errorHandler);
@@ -34,16 +38,17 @@ app.use(errorHandler);
 // Connect to MongoDB and start the server
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('MongoDB connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}.`);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected.");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}.`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
   });
-})
-.catch((err) => {
-  console.error('MongoDB connection error:', err.message);
-});
